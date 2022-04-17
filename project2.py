913bb7eca88fbf486006a6e423617c79492aafb5
ammaralimalik <ammaralimalik241@gmail.com>
2022-04-17 01:29:50 -0400

    Fixed files

--- project2.py
+++ project2.py
@@ -1,1 +1,1 @@
-import numpy as npimport pandas as pdfrom sklearn.cluster import KMeansfrom sklearn.feature_extraction.text import CountVectorizerdata = np.loadtxt("descriptions.txt", dtype="str", delimiter="\t", skiprows=1)index = data[:,0]textData = data[:,1]
+import numpy as npimport pandas as pdfrom sklearn.cluster import KMeansfrom sklearn.feature_extraction.text import CountVectorizerdata = np.loadtxt("descriptions.txt", dtype="str", delimiter="\t", skiprows=1)index = data[:,0]textData = data[:,1]
